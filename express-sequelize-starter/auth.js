5bd9eed2f8481e56b0d4bae082cd5cfcd11770f0
Jared B <jburnt17@gmail.com>
2021-11-11 14:13:18 -0500

    phase 2 done

--- express-sequelize-starter/auth.js
+++ express-sequelize-starter/auth.js
@@ -1,5 +1,8 @@
 const jwt = require("jsonwebtoken");
 const { jwtConfig } = require("./config");
+const bearerToken = require("express-bearer-token");
+const db = require("./db/models");
+const { User } = db;
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -21,4 +24,34 @@ const getUserToken = (user) => {
   return token;
 };
 
-module.exports = { getUserToken };
+const restoreUser = (req, res, next) => {
+  const { token } = req;
+  console.log(req)
+
+  if (!token) {
+    return res.set("WWW-Authenticate", "Bearer").status(401).end();
+  }
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    // TODO: Define asynchronous function for jwtPayload logic
+    if (err) {
+      err.status = 401;
+      return next(err);
+    }
+    const { id } = jwtPayload.data;
+
+    try {
+      req.user = await User.findByPk(id);
+    } catch (e) {
+      return next(e);
+    }
+
+    if (!req.user) {
+      return res.set("WWW-Authenticate", "Bearer").status(401).end();
+    }
+    return next();
+  });
+};
+const requireAuth = [bearerToken(), restoreUser];
+
+module.exports = { getUserToken, requireAuth };
